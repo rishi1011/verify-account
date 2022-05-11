1fc19a135a65ed2c21fc17bf841a413e10c90356
rishikrishna <r.rishikrishna@gmail.com>
2022-05-11 17:18:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputCodes = document.querySelectorAll('.code');
+
+inputCodes[0].focus();
+
+inputCodes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            if (idx === inputCodes.length - 1) {
+                if (code.value !== '')
+                    e.preventDefault();
+            } else {
+                setTimeout(() => inputCodes[idx + 1].focus(), 0);
+            }
+        } else if (e.key === 'Backspace') {
+            if (code.value === '') {
+                inputCodes[idx - 1].focus();
+            }
+        }
+    });
+});
